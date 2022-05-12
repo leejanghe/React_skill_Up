8c56b55c11ab410b271726a530a0f3c4af3197ff
leejanghe <babybye777@gmail.com>
2022-05-12 18:30:50 +0900

    router test

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Home from './router/Home';
+import Test1 from './router/Test1';
 
 function App() {
   return (
-    <div>hello world!</div>
+    
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+
+    <Routes>
+      <Route path="test1" element={<Test1/>}/>
+    </Routes>
+    </Router>
+    
   );
 }
 
