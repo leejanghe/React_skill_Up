437d03d1abe162e467e3ea36b08766de211a0f34
leejanghe <babybye777@gmail.com>
2022-05-13 00:50:33 +0900

    styledcomponent test

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavWrap = styled.div`
+background: blue;
+>a{
+    color: white;
+    text-decoration: none;
+}
+`
+
 function Navbar() {
     return (
-        <div>
+        <NavWrap>
             <Link to="/">Home</Link>
             <Link to="/test1">test link</Link>
-        </div>
+        </NavWrap>
     )
 }
 
