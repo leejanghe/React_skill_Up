058c4dc6e828e66502384f61efc09f3a35a931ae
leejanghe <babybye777@gmail.com>
2022-05-16 19:55:09 +0900

    data axios

--- myapp/src/components/paginate/PaginatePage.js
+++ myapp/src/components/paginate/PaginatePage.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import ReactPaginate from 'react-paginate'
+import axios from 'axios'
 
 function PaginatePage() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            setPosts(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    console.log(posts)
     return (
+        <>
         <div>
-            dafsfasfasdf
+            <h1>게시물 목록</h1>
         </div>
+        <div>
+            {posts.map(({id, title, body})=>(
+                <div>
+                    <h3>
+                    {id}. {title}
+                    </h3>
+                    <p>{body}</p>
+                </div>
+            ))}
+        </div>
+        </>
     )
 }
 
