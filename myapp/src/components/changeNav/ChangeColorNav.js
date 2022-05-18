500eb1541a136e84a5a6c4b63ab1b181c06d2b93
leejanghe <babybye777@gmail.com>
2022-05-19 01:05:59 +0900

    text 작업

--- myapp/src/components/changeNav/ChangeColorNav.js
+++ myapp/src/components/changeNav/ChangeColorNav.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
+import AlarmText from '../../common/AlarmText'
 
 function ChangeColorNav() {
+
+    const[togle, setTogle]=useState('')
+
+    const text = [{
+        h1:'상태에 따른 네브 변경 및 배너 text변경하기',
+        p:'useState 상태 변경함수와 css로 다양하게 네브 변경을 구현 할수있습니다.'
+    }]
+
     return (
         <div>
-            준비중...
+            <AlarmText text={text}/>
         </div>
     )
 }
