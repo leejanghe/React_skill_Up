543d74e68908fdb7f7912d6fa2c0cdcc2b40430d
leejanghe <babybye777@gmail.com>
2022-05-22 23:44:45 +0900

    done

--- myapp/src/components/paginate/Posts.js
+++ myapp/src/components/paginate/Posts.js
@@ -4,8 +4,20 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
 width: 100%;
 height: 60%;
+padding-top:1rem;
+padding-bottom:1rem;
 background: #212B36;
 color: #fff;
+>div>h3{
+  color:red;
+  font-size: 1rem;
+  padding: .3rem;
+}
+p{
+  color: fff;
+  font-size: 16px;
+  padding: .3rem;
+}
 `
 
 function Posts({posts, loading}) {
