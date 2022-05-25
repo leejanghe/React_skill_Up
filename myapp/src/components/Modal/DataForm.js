2cb5c030b2cc4977c1f0124f755463aea99f4bba
leejanghe <babybye777@gmail.com>
2022-05-25 20:40:19 +0900

    css done

--- myapp/src/components/Modal/DataForm.js
+++ myapp/src/components/Modal/DataForm.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+display: grid;
+gap: 30px;
+grid-template-columns: 1fr 1fr 1fr;
+margin-top: 2rem;
+>div{
+    background: white;
+    padding: 1rem;
+}
+`
 
 function DataForm({data}) {
     console.log('data',data)
     return (
-                <div>
+                <Wrapper>
                     {data.map(item => (
                         <div key={item.id}>
                             <p>{item.title}</p>
                             <p>{item.contents}</p>
+                            <p>자세히 보기</p>
                         </div>               
                     ))}    
-                </div>
+                </Wrapper>
     )
 }
 
