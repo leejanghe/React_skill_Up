57515bd9542d2979621222b97fcdcf49f6641764
leejanghe <babybye777@gmail.com>
2022-06-04 01:22:01 +0900

    none

--- myapp/src/components/pushData/PushDataPage.js
+++ myapp/src/components/pushData/PushDataPage.js
@@ -6,17 +6,18 @@ const [workImg, setWorkImg] = useState([])
 
 
     return (
-        <div>
-            <h3>이미지</h3>
-         <div>
-             {
-                 workImg.length > 0 ? workImg.map(item =>(
-                     <img src={item.thumbnail} /> 
-                 )):''
-             }
-             <div>이미지 추가</div>
-         </div>
-        </div>
+        // <div>
+        //     <h3>이미지</h3>
+        //  <div>
+        //      {
+        //          workImg.length > 0 ? workImg.map(item =>(
+        //              <img src={item.thumbnail} /> 
+        //          )):''
+        //      }
+        //      <div>이미지 추가</div>
+        //  </div>
+        // </div>
+        <h1>보류 그냥 삭제</h1>
     )
 }
 
